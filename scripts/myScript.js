8735526192bd728c7e19d70e6489cad7bd68f19e
thanhhungdesi <thanhhungdesi@gmail.com>
2017-07-13 22:47:02 +0700

    full up

---
+++ scripts/myScript.js
@@ -0,0 +1,24 @@
+$.noConflict();
+
+jQuery(document).ready(function() {
+
+});
+
+/*-- JavaScript --*/
+var now = new Date();
+
+var days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+var months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+
+var date = ((now.getDate() < 10) ? "0" : "") + now.getDate();
+
+function fourdigits(number) {
+    return (number < 1000) ? number + 1900 : number;
+}
+today = days[now.getDay()] + ", " +
+    months[now.getMonth()] + " " +
+    date + ", " +
+    (fourdigits(now.getYear()));
+
+document.getElementById('date-area').innerHTML = today;
