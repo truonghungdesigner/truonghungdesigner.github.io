e63bb91f0c0ebd573362252f9d57911367e8f451
thanhhungdesi <thanhhungdesi@gmail.com>
2017-07-21 12:41:01 +0700

    TEXT-DEF

--- scripts/myadmin.js
+++ scripts/myadmin.js
@@ -3,4 +3,19 @@ jQuery(document).ready(function() {
     jQuery('[data-toggle="offcanvas"]').click(function() {
         jQuery('#side-menu').toggleClass('hidden-xs')
     });
+
+    var config = {
+        '.chosen-select': {},
+        '.chosen-select-deselect': { allow_single_deselect: true },
+        '.chosen-select-no-single': { disable_search_threshold: 10 },
+        '.chosen-select-no-result': { no_results_text: '0ops, nothing found!' },
+        '.chosen-select-width': { width: "95%" }
+    }
+    for (var selector in config) {
+        jQuery(selector).chosen(config[selector]);
+    };
+
+    jQuery('.summernote').summernote({
+        height: 200
+    });
 });
